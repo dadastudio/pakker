7e13b8b1597b8762056c577a85a388583b648351
Michał Jarociński <farmazone@gmail.com>
2025-10-04 07:32:59 +0200

    Stage all changes, commit and push

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@ git pull origin main
 # composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 /usr/local/php83/bin/php $HOME/.local/bin/composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 if [ -f artisan ]; then
-php artisan migrate
+/usr/local/php83/bin/php artisan migrate
 
-php artisan config:clear
-php artisan cache:clear
+/usr/local/php83/bin/php artisan config:clear
+/usr/local/php83/bin/php artisan cache:clear
 
 
 fi
